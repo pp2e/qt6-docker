eafb631e3cd326023c5c38c8ae885decb0e9f0fb
AlTeveD2v <114943593+AlTeveD2v@users.noreply.github.com>
2024-04-25 16:33:38 +0300

    Update get_qt.sh

--- 6.6-wasm-aqt/get_qt.sh
+++ 6.6-wasm-aqt/get_qt.sh
@@ -29,6 +29,7 @@ echo
 echo '--> Download & install the Qt library using aqt'
 echo
 
+aqt list-tool linux desktop
 #aqt list-qt linux desktop --arch "$QT_VERSION"
 #aqt list-qt linux desktop --long-modules "$QT_VERSION" wasm_singlethread
 aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" wasm_singlethread -m qtmultimedia
