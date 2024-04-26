36693b445fc97ba1f2b5605c0fad8aae794ed15a
AlTeveD2v <114943593+AlTeveD2v@users.noreply.github.com>
2024-04-26 16:36:55 +0000

    С друзьями нужно здороваться и прощаться

--- 6.6-android-aqt/get_qt.sh
+++ 6.6-android-aqt/get_qt.sh
@@ -38,7 +38,7 @@ aqt install-tool -O "$QT_PATH" linux desktop tools_cmake
 # Host Qt needed for cross-compilation
 aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" gcc_64
 
-pip3 freeze | xargs pip3 uninstall -y
+pip3 freeze | xargs pip3 uninstall -y --break-system-packages
 
 # Create qt-cmake wrapper to simplify the android sdk usage
 mkdir -p /usr/local/bin
