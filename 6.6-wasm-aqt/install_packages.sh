d27ecefc687961e867d1f23550bd10cc97b1cf07
AlTeveD2v <114943593+AlTeveD2v@users.noreply.github.com>
2024-04-26 12:15:37 +0000

    Update install_packages.sh

--- 6.6-wasm-aqt/install_packages.sh
+++ 6.6-wasm-aqt/install_packages.sh
@@ -33,6 +33,9 @@ echo
 
 cat /tmp/to_install_libs.lst | cut -d: -f 1 | xargs apt install -y --no-install-suggests --no-install-recommends $ADDITIONAL_PACKAGES
 
+# Install ninja
+apt install ninja
+
 # Complete the cleaning
 
 apt -qq clean
