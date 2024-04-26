e6536b8e2fde19af893cb18673ab205f11c25938
AlTeveD2v <114943593+AlTeveD2v@users.noreply.github.com>
2024-04-26 13:19:32 +0000

    Update install_packages.sh

--- 6.6-android-aqt/install_packages.sh
+++ 6.6-android-aqt/install_packages.sh
@@ -34,7 +34,7 @@ echo
 cat /tmp/to_install_libs.lst | cut -d: -f 1 | xargs apt install -y --no-install-suggests --no-install-recommends $ADDITIONAL_PACKAGES
 
 # Install ninja
-apt install ninja-build
+apt install ninja-build extra-cmake-modules
 
 # Complete the cleaning
 
