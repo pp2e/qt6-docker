27165462ea6fdd16c4c16bf3123d810f4ce19d1c
Женёчек <nuromskij_em_0819@1511.ru>
2024-05-10 11:42:50 +0300

    эй посмотри лучи солнца сделай вид

--- 6.7-android-aqt/get_qt.sh
+++ 6.7-android-aqt/get_qt.sh
@@ -29,10 +29,10 @@ echo
 echo '--> Download & install the Qt library using aqt'
 echo
 
-aqt list-qt linux android
-aqt list-tool linux desktop
+aqt list-qt all_os android
+aqt list-tool all_os desktop
 for abi in armv7 arm64_v8a x86 x86_64; do
-    aqt install-qt -O "$QT_PATH" linux android "$QT_VERSION" "android_$abi" -m qtshadertools
+    aqt install-qt -O "$QT_PATH" all_os android "$QT_VERSION" "android_$abi" -m qtshadertools
 done
 aqt install-tool -O "$QT_PATH" linux desktop tools_cmake
 #aqt install-tool -O "$QT_PATH" linux desktop tools_ninja
