3778cce8aae2d76460c352178ad04ec4f9d40189
Женёчек <nuromskij_em_0819@1511.ru>
2024-05-10 12:38:12 +0300

    я изгибаю измерения машина шелби

--- 6.7-android-aqt/get_qt.sh
+++ 6.7-android-aqt/get_qt.sh
@@ -32,9 +32,9 @@ echo
 # aqt list-qt all_os android
 # aqt list-tool all_os desktop
 for abi in armv7 arm64_v8a x86 x86_64; do
-    aqt install-qt -O "$QT_PATH" all_os android "$QT_VERSION" "android_$abi" -m qtshadertools
+    aqt install-qt -O "$QT_PATH" linux android "$QT_VERSION" "android_$abi" -m qtshadertools
 done
-aqt install-tool -O "$QT_PATH" all_os desktop tools_cmake
+aqt install-tool -O "$QT_PATH" linux desktop tools_cmake
 #aqt install-tool -O "$QT_PATH" linux desktop tools_ninja
 # Host Qt needed for cross-compilation
 aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" gcc_64 -m qtshadertools
